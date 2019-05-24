56a73d5446411d1814428cd8f685cab26521a599
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-24 17:48:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(num)
   false if num < 2
   (2...num).each do |number|
-    if (num % number).zero? 
+    if (num % number).zero?
       return false
     end
   end
-  
-    true 
+
+    true
   end
 end
